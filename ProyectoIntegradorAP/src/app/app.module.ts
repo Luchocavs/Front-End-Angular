2e94b52cddb0271733bc06cfcfe17ae655142b61
Luchocavs <lucianoalvarez@gmail.com>
2022-06-01 14:22:26 -0300

    Creacion componente header y Educacion

--- ProyectoIntegradorAP/src/app/app.module.ts
+++ ProyectoIntegradorAP/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './Componentes/header/header.component';
+import { EducacionComponent } from './Componentes/educacion/educacion.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    EducacionComponent
   ],
   imports: [
     BrowserModule,
